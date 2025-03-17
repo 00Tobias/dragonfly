616f70d5d4f30f3b350e98c7f7c8bfae0424e450
tobias <tobias@nights.rest>
2025-03-17 23:33:25 +0100

    Rename div id to dragonflyWindowContent

--- src/dragonfly/prelude.cljs
+++ src/dragonfly/prelude.cljs
@@ -3,5 +3,5 @@
    ["three/build/three.module.js" :as three]))
 
 (defn init-prelude []
-  (def ^js container (.getElementById js/document "dragonfly"))
+  (def ^js container (.getElementById js/document "dragonflyWindowContent"))
   (def ^js scene (three/Scene.)))
